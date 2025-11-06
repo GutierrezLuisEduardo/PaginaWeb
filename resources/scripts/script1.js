53ca860dad1f97797876eb0d2e1558a11f16a321
Luis Gtz <164699872+GutierrezLuisEduardo@users.noreply.github.com>
2025-11-06 00:04:02 -0600

    Añade la función que actualiza h1 con nombre de poder

--- resources/scripts/script1.js
+++ resources/scripts/script1.js
@@ -1,26 +1,33 @@
 function cambiarPower(power) {
     const personaje = document.getElementById("personaje");
+    const titular = document.getElementById("titular");
 
     switch (power) {
         case 1:
             personaje.src = "../img/power1.png";
             document.body.style.backgroundColor = "green";
+            titular.textContent = "Sentido Arácnido";
             break;
         case 2:
             personaje.src = "../img/power2.png";
             document.body.style.backgroundColor = "red";
+            titular.textContent = "Agilidad Mejorada";
             break;
         case 3:
             personaje.src = "../img/power3.png";
             document.body.style.backgroundColor = "blue";
+            titular.textContent = "Arrojar Telarañas";
             break;
         case 4:
             personaje.src = "../img/power4.png";
             document.body.style.backgroundColor = "white";
+            titular.textContent = "Escalada de Muro";
             break;
         default:
-            personaje.src = "img/personaje_base.png";
-            document.body.style.backgroundImage = "";
+            personaje.src = "../img/spiderman.png";
+            document.body.style.backgroundColor = "";
+            titular.textContent = "Spiderman";
+
     }
 
     // Ajustar estilos del fondo
